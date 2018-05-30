9752c9c0b5d94342e5fda3fc7aaa2b02e471a367
denniscual <mamaylovepapay@outlook.com>
2018-05-30 12:42:04 +0800

    add supports for jest as test runner

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world irish jane')
+/**
+ * add :: (Number, Number) => Number
+ *
+ * Add the 2 inputs
+ * @param {Number} x
+ * @param {Number} y
+ * @return {Number} Total number
+ */
+export const add = (x, y) => x * y
