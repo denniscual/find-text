10ed1a79d3c7c46e63d6ef2c33656d2bd26a3d72
denniscual <mamaylovepapay@outlook.com>
2018-05-31 22:28:27 +0800

    add uni tests for findTextInFiles API

---
+++ __tests__/data/fake3.php
@@ -0,0 +1,1 @@
+// TODO: Fake data for js.
--- __tests__/index.js
+++ __tests__/index.js
@@ -1,32 +1,51 @@
-// January 01, 2018
 import { spy } from 'sinon'
-import { findText } from '../src'
+import { findText, findTextInFiles } from '../src'
 import fs from 'fs'
 
 // This will generate an input to findText
 const fakeData = ({
   word = 'TODO',
-  path = `${process.cwd()}/__tests__/fake.txt`
+  path = `${process.cwd()}/__tests__/data/fake.txt`
 }) => ({
   word,
   path
 })
 
-// This will generate a fake result for findText
-const fakeDataFromFile = () => ({
-  fileName: '__tests__/fake.txt',
-  matches: [
-    {
-      text: '// TODO: This line should be included',
+// This will generate a fake result for findText and findTextInFiles
+const fakeDataFromFiles = () => [
+  {
+    fileName: '__tests__/data/fake.txt',
+    matches: [
+      {
+        text: '// TODO: This line should be included',
+        lineNumber: 1
+      },
+      {
+        text: '// TODO: And also this line',
+        lineNumber: 3
+      }
+    ],
+    count: 2
+  },
+  {
+    fileName: '__tests__/data/fake2.txt',
+    matches: [
+      {
+        text: '// TODO: Sample fake text 1',
+        lineNumber: 1
+      }
+    ],
+    count: 1
+  },
+  {
+    fileName: '__tests__/data/fake3.php',
+    matches: [{
+      text: '// TODO: Fake data for js.',
       lineNumber: 1
-    },
-    {
-      text: '// TODO: And also this line',
-      lineNumber: 3
-    }
-  ],
-  count: 2
-})
+    }],
+    count: 1
+  }
+]
 
 describe('findText', function () {
   beforeEach(() => {
@@ -62,7 +81,7 @@ describe('findText', function () {
     expect.assertions(1)
     return findText(word, path)
       .then((result) => {
-        const expected = fakeDataFromFile()
+        const expected = fakeDataFromFiles()[0]
         // assertions
         expect(result).toEqual(expected)
       })
@@ -79,6 +98,53 @@ describe('findText', function () {
   })
 })
 
-// TODO: Add more unit tests for findTextInFiles.
-describe.skip('findTextInFiles', () => {
+const fakeDataForFindTextInFiles = ({
+  word = 'TODO',
+  pattern = '__tests__/**/*.txt'
+}) => ({
+  word,
+  pattern
+})
+
+describe('findTextInFiles', () => {
+  it('should return empty array if there is no word found', () => {
+    const { word, pattern } = fakeDataForFindTextInFiles({word: 'FIXME'})
+    expect.assertions(1)
+    return findTextInFiles(word, pattern)
+      .then((result) => {
+        expect(result).toEqual([])
+      })
+  })
+
+  it('should return empty array when there is no matched pattern', () => {
+    const { word, pattern } = fakeDataForFindTextInFiles({pattern: 'dummy/path'})
+    expect.assertions(1)
+    return findTextInFiles(word, pattern)
+      .then((result) => {
+        expect(result).toEqual([])
+      })
+  })
+
+  it('should read multiple types of file based in the given patterns and return an equal result', () => {
+    // passing multiple patterns
+    const { word, pattern } = fakeDataForFindTextInFiles({pattern: ['__tests__/**/*.txt', '__tests__/**/*.php']})
+    expect.assertions(1)
+    return findTextInFiles(word, pattern)
+      .then((result) => {
+        const expected = fakeDataFromFiles()
+        expect(result).toEqual(expected)
+      })
+  })
+
+  it('should skip file which is not matched in the given pattern', () => {
+    // passing pattern for only .txt file
+    const { word, pattern } = fakeDataForFindTextInFiles({pattern: '__tests__/**/*.txt'})
+    expect.assertions(1)
+    return findTextInFiles(word, pattern)
+      .then((result) => {
+        // get the object which is generated for .php file
+        const expected = fakeDataFromFiles()[3]
+        expect(result).not.toContainEqual(expected)
+      })
+  })
 })
